4395f7a2bc83fc360a3a9f7fdec628aff1b959ce
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-05-01 20:44:27 +0700

    update

--- team/index.php
+++ team/index.php
@@ -1,1 +1,31 @@
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum odit, ut maxime labore error voluptate. Error, ex fugiat. Dignissimos similique nihil adipisci pariatur ullam dolorem ratione quis ea debitis non.
+<?php
+    // Bagian pertama
+    echo "Ini adalah bagian pertama dari skrip PHP.\n";
+    echo "Ini masih bagian pertama.\n";
+    echo "Bagian pertama hampir selesai.\n";
+?>
+
+<?php
+    // Mencetak "spam" dari baris 1 hingga 100 dengan variasi syntax
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo "SpamEgg ";
+        } elseif ($i % 2 == 0) {
+            echo "spam ";
+        } else {
+            echo "SPAM ";
+        }
+
+        if ($i % 10 == 0) {
+            echo "($i)\n";
+        } else {
+            echo "\n";
+        }
+    }
+?>
+
+<?php
+    // Bagian kedua
+    echo "Ini adalah bagian kedua dari skrip PHP.\n";
+    echo "Ini bagian kedua setelah mencetak spam.\n";
+?>
--- team/rifandi.php
+++ team/rifandi.php
@@ -1,1 +1,31 @@
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate laboriosam impedit deleniti alias, dignissimos ducimus hic obcaecati atque expedita ratione molestiae esse porro, dolore quidem inventore officia animi, quos doloribus!
+<?php
+    // Bagian pertama
+    echo "Ini adalah bagian pertama dari skrip PHP.\n";
+    echo "Ini masih bagian pertama.\n";
+    echo "Bagian pertama hampir selesai.\n";
+?>
+
+<?php
+    // Mencetak "spam" dari baris 1 hingga 100 dengan variasi syntax
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo "SpamEgg ";
+        } elseif ($i % 2 == 0) {
+            echo "spam ";
+        } else {
+            echo "SPAM ";
+        }
+
+        if ($i % 10 == 0) {
+            echo "($i)\n";
+        } else {
+            echo "\n";
+        }
+    }
+?>
+
+<?php
+    // Bagian kedua
+    echo "Ini adalah bagian kedua dari skrip PHP.\n";
+    echo "Ini bagian kedua setelah mencetak spam.\n";
+?>
