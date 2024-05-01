f400b58e3fb2c883340686607456d14ee3054185
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-05-01 20:22:43 +0700

    update

--- resources/views/servis.blade.php
+++ resources/views/servis.blade.php
@@ -179,7 +179,7 @@
             </a>
 
             <a href="{{ route('contact') }}" class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                contact
+                Contact
             </a>
 
             <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
